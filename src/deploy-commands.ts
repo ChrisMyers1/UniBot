3336fe394a7243250b14c875704ca1d59236f2ed
Chris Myers <chris.myers99@yahoo.com>
2026-07-07 14:39:17 -0400

    Add guild command development support

--- src/deploy-commands.ts
+++ src/deploy-commands.ts
@@ -5,28 +5,51 @@ import path from "path";
 
 async function main() {
   const commandsPath = path.join(__dirname, "commands");
+
   const commandFiles = fs
     .readdirSync(commandsPath)
     .filter((f) => f.endsWith(".ts") || f.endsWith(".js"));
 
   const commandData = [];
+
   for (const file of commandFiles) {
     const command = require(path.join(commandsPath, file));
-    if (command.data) commandData.push(command.data.toJSON());
+
+    if (command.data) {
+      commandData.push(command.data.toJSON());
+    }
   }
 
   const rest = new REST().setToken(process.env.DISCORD_TOKEN!);
 
-  console.log(`Registering ${commandData.length} global command(s)...`);
+  const clientId = process.env.CLIENT_ID!;
+  const guildId = process.env.GUILD_ID;
 
-  await rest.put(Routes.applicationCommands(process.env.CLIENT_ID!), {
+  let route;
+
+  if (guildId) {
+    console.log(
+      `Registering ${commandData.length} guild command(s) to ${guildId}...`
+    );
+
+    route = Routes.applicationGuildCommands(clientId, guildId);
+  } else {
+    console.log(`Registering ${commandData.length} global command(s)...`);
+
+    route = Routes.applicationCommands(clientId);
+  }
+
+  await rest.put(route, {
     body: commandData,
   });
 
-  console.log("Done. Global commands can take up to an hour to propagate.");
-  console.log(
-    "Tip: for instant updates while developing, register per-guild instead using Routes.applicationGuildCommands(clientId, guildId)."
-  );
+  if (guildId) {
+    console.log("Done. Guild commands should update instantly.");
+  } else {
+    console.log(
+      "Done. Global commands can take up to an hour to propagate."
+    );
+  }
 }
 
 main().catch(console.error);
